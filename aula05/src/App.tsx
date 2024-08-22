2c6ec9a636950069e1f15c571b1bc8f361383feb
brunosz2712 <brunosouza_27@yahoo.com.br>
2024-08-22 17:18:14 -0300

    Boyler plate pronto para o exemplo de PROPS.

--- aula05/src/App.tsx
+++ aula05/src/App.tsx
@@ -1,7 +1,7 @@
-export default function App(){
-  return (
-    <div> 
-      <h1> Componente App</h1>
+export default function app(){
+  return(
+    <div>
+      <h1>Componente App</h1>
     </div>
   );
 }
