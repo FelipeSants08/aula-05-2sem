1f70e4d6b6403dab9d380090c3f91b19a122a60f
brunosz2712 <brunosouza_27@yahoo.com.br>
2024-08-22 18:26:30 -0300

    Passando PROPS por variavel.

--- aula05/src/App.tsx
+++ aula05/src/App.tsx
@@ -1,7 +1,22 @@
+import Cabecalho from "./Cabecalho/Cabecalho";
+
 export default function app(){
+
+  const titulo:string = "Minha Página Principal!";
+
+
+
   return(
     <div>
       <h1>Componente App</h1>
+      <Cabecalho tituloProps = {titulo}/>
     </div>
   );
 }
+
+// const numero:number = 2;
+  // const listaNomes:string[] = ["Bruno", "Paulo", "Marcos"];
+  // const listaNomes2:[string,number,string] = ["Bruno", 1, "Marcos"];
+  // const elementoDiv:HTMLDivElement = document.createElement('div');
+  // const objeto:{nome:string, idade:number,} = {nome: "Bruno", idade: 22};
+  // const objeto2:{nome:string, idade:number, isEstudante?:boolean} = {nome: "Paulo", idade: 32, isEstudante:true};
