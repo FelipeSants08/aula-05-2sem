815f7083292501d45762c157280a1477bb2171bb
anacarolcg0 <anacarolcg0@gmail.com>
2024-08-22 12:13:00 -0300

    Adicionando valores para o intersection.

---
+++ aula05/src/Cabecalho/Cabecalho.tsx
@@ -0,0 +1,20 @@
+type CabecalhoProps = {
+    paginaProps:string;
+    nrPaginaProps:number | string;
+    statusProps: "loading" | "deployed" | "xuxu";
+    avisoProps:(Function);
+}
+
+export default function Cabecalho({paginaProps,nrPaginaProps,statusProps,avisoProps}:CabecalhoProps){
+
+    document.title = statusProps+" - "+nrPaginaProps;
+
+    return (
+        <header>
+            <h1>{paginaProps+" - "+nrPaginaProps}</h1>
+            <div>
+                <button onClick={()=> avisoProps()}>Aviso do pai</button>
+            </div>
+        </header>
+    );
+}
