9c73e8fe1fcc86b29689d4f3c90e857f47a9d868
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2024-08-22 11:40:54 -0300

    Passando função como PROPS.

--- aula05/src/App.tsx
+++ aula05/src/App.tsx
@@ -3,11 +3,12 @@ import Cabecalho from "./Cabecalho/Cabecalho";
 export default function App(){
 
   const titulo:string = "Minha página principal!";
- 
+  const aviso = ()=> alert(titulo);
+
   return (
     <div>
       <h1>Componente App</h1>
-      <Cabecalho tituloProps={titulo}/>
+      <Cabecalho tituloProps={titulo} avisoProps={aviso}/>
     </div>
   );
 }
--- aula05/src/Cabecalho/Cabecalho.tsx
+++ aula05/src/Cabecalho/Cabecalho.tsx
@@ -1,10 +1,13 @@
-export default function Cabecalho(props:{tituloProps:string}){
+export default function Cabecalho(props:{tituloProps:string, avisoProps:Function}){
 
     document.title = props.tituloProps;
 
     return (
         <header>
             <h1>{props.tituloProps}</h1>
+            <div>
+                <button onClick={()=> props.avisoProps()}>Aviso do pai</button>
+            </div>
         </header>
     );
 }
