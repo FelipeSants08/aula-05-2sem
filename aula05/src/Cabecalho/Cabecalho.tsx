6e2a3efed6a1574c5fba7f4babb723b7469b99bb
jenniesuzuki <jenniferkaorisuzuki@gmail.com>
2024-08-22 11:51:46 -0300

    Desestruturando objeto props e utilizando ...rest

--- aula05/src/Cabecalho/Cabecalho.tsx
+++ aula05/src/Cabecalho/Cabecalho.tsx
@@ -1,15 +1,49 @@
-export default function Cabecalho(props:{tituloProps:string, avisoProps:Function}) {
+export default function Cabecalho({tituloProps, avisoProps}:{tituloProps:string, avisoProps:Function}) {
     
-    document.title = props.tituloProps;
-    
-    addEventListener("click", ()=> {})
+    document.title = tituloProps;
+
+    // addEventListener("click", ()=> {})
 
     return(
         <header>
-            <h1>{props.tituloProps}</h1>
+            <h1>{tituloProps}</h1>
             <div>
-                <button onClick={()=> props.avisoProps()}>Aviso do pai</button>
+                <button onClick={()=> avisoProps()}>Aviso do pai</button>
             </div>
         </header>
-    )
+    );
 }
+
+
+
+
+// export default function Cabecalho(props:{tituloProps:string, avisoProps:Function}) {
+    
+//     document.title = props.tituloProps;
+    
+//     const aluno: {nome:string,idade:number,isAluno:boolean,nacionalidade:string} = {
+//         nome: 'João',
+//         idade: 20,
+//         isAluno: true,
+//         nacionalidade:"brasileiro"
+//     }
+
+//     const {nome, idade, ...resto} = aluno;
+
+//     // addEventListener("click", ()=> {})
+
+//     return(
+//         <header>
+//             <h1>{props.tituloProps}</h1>
+//             <div>
+//                 <button onClick={()=> props.avisoProps()}>Aviso do pai</button>
+//             </div>
+//             <div>
+//                 <p>Nome: {nome}</p>
+//                 <p>Idade: {idade}</p>
+//                 <p>É aluno: {resto.isAluno.toString()}</p>
+//                 <p>Nacionalidade: {resto.nacionalidade}</p>
+//             </div>
+//         </header>
+//     );
+// }
