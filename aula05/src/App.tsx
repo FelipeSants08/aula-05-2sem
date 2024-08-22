7aaa28c5cd33756431b5f4b3e98fc46153c402ba
anacarolcg0 <anacarolcg0@gmail.com>
2024-08-22 12:12:40 -0300

    Union-types e Intersection de valores.

---
+++ aula05/src/App.tsx
@@ -0,0 +1,16 @@
+import Cabecalho from "./Cabecalho/Cabecalho";
+
+export default function App(){
+
+  const pagina:string = "Minha página principal!";
+  const nrPagina:string = "DOIS";
+  const status = "xuxu";
+  const aviso = ()=> alert(pagina);
+
+  return (
+    <div>
+      <h1>Componente App</h1>
+      <Cabecalho paginaProps={pagina} nrPaginaProps={nrPagina} statusProps={status} avisoProps={aviso}/>
+    </div>
+  );
+}
