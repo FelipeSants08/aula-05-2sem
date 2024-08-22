3ff72f3045f7e39ad1912e19cb8e6205ffe78ef1
Felipe-2833 <felipe.elucidator@gmail.com>
2024-08-22 11:28:24 -0300

    Props

---
+++ Aula-05/src/App.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Cabecalho from './Cabecalho/Cabecalho'
+
+export default function App() {
+
+  const titulo:string = "Minha página principal!"
+ // const numero:number = 2
+  //const listaDeNomes: string[] = ["João", "Maria", "Pedro"]
+  //const listaDeNomes2: [string,number,string] = ["João", 2, "Pedro"]
+ //const objeto: {nome:string,idade:number} = {nome: "João", idade: 25}
+  //const objeto2: {nome:string,idade:number, isEstudante?: boolean} = {nome: "Maria", idade:30, isEstudante:true}
+  //const elementoDiv: HTMLDivElement = document.createElement('div')
+
+  return (
+    <div>
+      <h1>Componete App</h1>
+      <Cabecalho tituloProps={titulo}/>
+    </div>
+  )
+}
+
