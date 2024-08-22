e8ed37ba6386f53fe19a45d896e025b4921e40a3
jenniesuzuki <jenniferkaorisuzuki@gmail.com>
2024-08-22 11:28:36 -0300

    Passando props por variável

--- aula05/src/App.tsx
+++ aula05/src/App.tsx
@@ -1,7 +1,19 @@
+import Cabecalho from "./Cabecalho/Cabecalho";
+
 export default function App() {
+
+  const titulo:string = "Minha página principal!";
+  // const numero:number = 2;
+  // const listaDeNomes:string[] = ["João", "Maria", "Pedro"];
+  // const listaDeNomes2:[string,number,string] = ["João", 1, "Pedro"];
+  // const elementoDiv:HTMLDivElement = document.createElement('div');
+  // const objeto:{nome:string,idade: number} = {nome: "João", idade: 25};
+  // const objeto2:{nome:string,idade: number, isEstudante?:boolean} = {nome: "Maria", idade: 30}; // O ?: significa que é opcional
+
   return(
     <div>
       <h1>Componente App</h1>
+      <Cabecalho tituloProps={titulo}/>
     </div>
   );
 }
