50e3bef3b5d5cef7758be48457a2133aa2646204
victoraugustogfavaro <victoraugustogfavaro@gmail.com>
2024-08-22 11:41:05 -0300

    Passando função como PROPS.

--- aula05/src/App.tsx
+++ aula05/src/App.tsx
@@ -1,7 +1,14 @@
-export default function App(){
-  return(
+import Cabecalho from "./Cabecalho/Cabecalho";
+
+export default function App() {
+
+  const titulo:string = "Minha página principal!";
+  const aviso = ()=> alert(titulo);
+  
+  return (
     <div>
       <h1>Componente App</h1>
+      <Cabecalho tituloProps={titulo} avisoProps={aviso} />
     </div>
-  )
+  );
 }
