ec9a0281f9398eb98eb61764952311e5fb2420dc
victoraugustogfavaro <victoraugustogfavaro@gmail.com>
2024-08-22 12:13:18 -0300

    Union-types e Intersection de valores.

--- aula05/src/Cabecalho/Cabecalho.tsx
+++ aula05/src/Cabecalho/Cabecalho.tsx
@@ -1,17 +1,19 @@
 type CabecalhoProps = {
-  tituloProps: string;
-  avisoProps: Function;
+  paginaProps: string;
+  nrPaginaProps: number | string;
+  statusProps: "loading" | "deployed";
+  avisoProps:(Function);
 };
 
-export default function Cabecalho({tituloProps, avisoProps}:CabecalhoProps) {
+export default function Cabecalho({paginaProps, nrPaginaProps, statusProps, avisoProps}:CabecalhoProps) {
   
-  document.title = tituloProps;
+  document.title = statusProps+" - "+nrPaginaProps;
 
   return (
     <header>
-      <h1>{tituloProps}</h1>
+      <h1>{paginaProps+" - "+nrPaginaProps}</h1>
       <div>
-        <button onClick={() => avisoProps()}>Aviso do pai</button>
+          <button onClick={() => avisoProps()}>Aviso do pai</button>
       </div>
     </header>
   );
