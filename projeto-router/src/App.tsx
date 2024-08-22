b9506a6346f57a57ef342a54df49a2e8c70096ff
vitin46 <viegidulira@gmail.com>
2024-08-22 11:28:24 -0300

    Passando props por variavel.

--- projeto-router/src/App.tsx
+++ projeto-router/src/App.tsx
@@ -1,7 +1,17 @@
+import Cabecalho from "./Cabecalho/Cabecalho";
+
 export default function App(){
+  
+  const titulo:string = "Minha pagina principal!";
+  // const listaDeNomes:string[] = ["joao", "maria", "nelson"];
+  // const listaDeNomes2:[string,number,string] = ["joao", 5, "nelson"];
+  // const objeto:{nome:string,idade:number} = {nome: "maria",idade:20};
+  // const objeto2:{nome:string,idade?:number} = {nome: "maria"};
+
   return(
     <div>
-      <h1>Componente App</h1>
+      <h1>Componente APP</h1>
+      <Cabecalho tituloProps={titulo}/>
     </div>
   );
 }
