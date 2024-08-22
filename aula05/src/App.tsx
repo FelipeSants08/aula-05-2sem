9c9473fd323b236bb912a65f452bf355b36fbee7
MarceloScoleso <marcelo.scolesojr@gmail.com>
2024-08-22 11:28:35 -0300

    props

--- aula05/src/App.tsx
+++ aula05/src/App.tsx
@@ -1,7 +1,13 @@
+import Cabecalho from "./Cabecalho/Cabecalho";
+
 export default function App(){
+
+  const titulo:string = "Minha página principal!";
+
   return (
     <div> 
       <h1> Componente App</h1>
+      <Cabecalho tituloProps={titulo}/>
     </div>
   );
 }
