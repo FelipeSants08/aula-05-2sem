3b5638ab76d9090b6aca2152e14fd47c4b9ad4bb
Felipe-2833 <felipe.elucidator@gmail.com>
2024-08-23 08:47:05 -0300

    interface

--- Aula-05/src/types.ts
+++ Aula-05/src/types.ts
@@ -1,3 +1,12 @@
+//interface é melhor usada para classes
+// export interface CabecalhoProps {
+//     paginaProps:string;
+//     nrPaginaProps: number | string;
+//     statusProps: "loading" | "deployed";
+//     avisoProps:()=> void;
+//     children:React.ReactNode;
+// }
+
 export type CabecalhoProps = {
     paginaProps:string;
     nrPaginaProps: number | string;
